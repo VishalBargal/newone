0eaa0e8d00e7aad99b9173d8c2d4d534c79d8c07
vishal <bargalvishal@gmail.com>
2023-10-28 16:01:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is my first project on the github
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
